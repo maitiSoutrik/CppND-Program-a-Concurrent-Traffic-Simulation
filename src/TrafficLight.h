3e8dd00dbb9f3938bb3aff67bd324339834652b2
maitiSoutrik <soutrikmaiti.uta@gmail.com>
2021-08-08 23:24:11 +0000

    cycleThroughPhases private method also added

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -50,7 +50,7 @@ public:
 private:
     TrafficLightPhase _currentPhase;
     // typical behaviour methods
-
+    void cycleThroughPhases();
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
