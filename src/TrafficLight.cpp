d05ba119503cd6ed8293a250f935a4fd17312412
maitiSoutrik <soutrikmaiti.uta@gmail.com>
2021-08-09 04:18:10 +0000

    Code added for task FP2

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -2,6 +2,13 @@
 #include <random>
 #include "TrafficLight.h"
 
+// random device will seed the generator
+std::random_device seeder;
+// make a Mersenne twister engine
+std::mt19937 engine(seeder());
+// distribution
+std::uniform_int_distribution<int> dist(4, 6);
+
 /* Implementation of class "MessageQueue" */
 
 /* 
@@ -43,7 +50,13 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    std::unique_lock<std::mutex> lck(_mutex);
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class.
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
+}
+
+double TrafficLight::generateCycleDuration(){
+    return dist(engine);
 }
 
 // virtual function which is executed in a thread
@@ -53,5 +66,34 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    
+    std::unique_lock<std::mutex> lck(_mutex);
+
+    int cycleDuration = generateCycleDuration();
+    std::chrono::time_point<std::chrono::system_clock> t1;
+    // init stop watch
+    t1 = std::chrono::system_clock::now();
+
+    while(true){
+
+        // sleep at every iteration to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        // compute the difference to stop watch
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>
+                                    (std::chrono::system_clock::now() - t1).count();
+
+        if(timeSinceLastUpdate >= cycleDuration){
+            // toggle between red and green
+            if(_currentPhase == red) _currentPhase = green;
+            else _currentPhase = red;
+            t1 = std::chrono::system_clock::now();
+            cycleDuration = generateCycleDuration();
+
+        }
+        
+
+    }
+
 }
 
