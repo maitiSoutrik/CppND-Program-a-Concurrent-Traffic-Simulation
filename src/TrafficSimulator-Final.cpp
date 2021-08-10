64b8d12ef526286de65fc40e094afc99221adf46
maitiSoutrik <soutrikmaiti.uta@gmail.com>
2021-08-10 21:03:36 +0000

    Changed code to use NYC Map

--- src/TrafficSimulator-Final.cpp
+++ src/TrafficSimulator-Final.cpp
@@ -123,7 +123,7 @@ int main()
     std::vector<std::shared_ptr<Vehicle>> vehicles;
     std::string backgroundImg;
     int nVehicles = 6;
-    createTrafficObjects_Paris(streets, intersections, vehicles, backgroundImg, nVehicles);
+    createTrafficObjects_NYC(streets, intersections, vehicles, backgroundImg, nVehicles);
 
     /* PART 2 : simulate traffic objects */
 
